8b6a57a97d1e8ddee372e9fd661ef4dbfc11e4fb
Alex Morega <alex@grep.ro>
2014-02-22 14:31:56 +0200

    load existing data

--- biblioteci.py
+++ biblioteci.py
@@ -1,12 +1,22 @@
 import flask
 from flask.ext.script import Manager
+import requests
+
+BIBLIOTECI_URL = ('https://raw2.github.com/mgax/biblioteci/'
+                  'master/biblioteci.geojson')
 
 views = flask.Blueprint('views', __name__)
 
 
 @views.route('/')
 def home():
-    return 'hi'
+    return flask.render_template('home.html')
+
+
+@views.route('/data')
+def data():
+    data = requests.get(BIBLIOTECI_URL).content
+    return flask.Response(data, content_type='application/json')
 
 
 def create_app():
