86933e90ad3fe1e8af28bc0f8b66fd2d0f562ad4
Alex Morega <alex@grep.ro>
2014-02-24 16:49:26 +0200

    log format

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ from biblioteci import create_app, create_manager
 
 def main():
     import logging
-    logging.basicConfig(level=logging.DEBUG)
+    log_format = "[%(asctime)s] %(module)s %(levelname)s %(message)s"
+    logging.basicConfig(level=logging.DEBUG, format=log_format)
     logging.getLogger('werkzeug').setLevel(logging.INFO)
     app = create_app()
     manager = create_manager(app)
